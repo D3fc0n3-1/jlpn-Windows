2bbe7fdda5c7c6c702155ad16c0f86d68d4c0627
D3fc0n3-1 <dfcn3.1@gmail.com>
2025-02-03 22:23:25 -0600

    Update jlpnz_Launcher.cpp
    Addeed error handling
    Added resource cleanup before exiting
    Added message loop to handle windows messages

--- jlpnz_Launcher.cpp
+++ jlpnz_Launcher.cpp
@@ -2,22 +2,39 @@
 #include <iostream>
 #include <string>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    // Create a window
+    const char CLASS_NAME[] = "JNLP Launcher";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    if (!RegisterClass(&wc))
+    {
+        std::cerr << "Failed to register window class" << std::endl;
+        return 1;
+    }
+
     HWND hWnd = CreateWindowEx(
         0,
-        "STATIC",
+        CLASS_NAME,
         "JNLP Launcher",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        300,
-        100,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
+        CW_USEDEFAULT, CW_USEDEFAULT, 300, 100,
+        NULL, NULL, hInstance, NULL
     );
 
     if (!hWnd)
@@ -26,20 +43,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
-    // Create a edit control for the URL
+    ShowWindow(hWnd, nCmdShow);
+
     HWND urlEdit = CreateWindowEx(
         0,
         "EDIT",
         "",
         WS_CHILD | WS_VISIBLE | WS_BORDER,
-        10,
-        10,
-        250,
-        20,
-        hWnd,
-        (HMENU)1,
-        hInstance,
-        NULL
+        10, 10, 250, 20,
+        hWnd, (HMENU)1, hInstance, NULL
     );
 
     if (!urlEdit)
@@ -48,17 +60,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
-    // Create a button to launch the JNLP
     HWND launchButton = CreateWindowEx(
         0,
         "BUTTON",
         "Launch",
         WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        10,
-        40,
-        100,
-        20,
-        hWnd,
-        (HMENU)2,
-        hInstance,
-       
+        10, 40, 100, 20,
+        hWnd, (HMENU)2, hInstance, NULL
+    );
+
+    if (!launchButton)
+    {
+        std::cerr << "Failed to create button" << std::endl;
+        return 1;
+    }
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
